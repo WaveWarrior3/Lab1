298f16bd643cfe09fe604488aa9592a1616e089b
Jacob <wavewarrior488@gmail.com>
2021-01-21 12:09:39 -0800

    Added hyperparameters to sim.py

---
+++ sim.py
@@ -0,0 +1,8 @@
+# Hyperparameters (all lengths in meters)
+L = 10  # Length of arena
+W = 10  # Width of arena
+d = 0.05    # wheel diameter
+w = 0.09    # width of robot
+delta_t = 0.1   # time step in seconds
+x0 = 0
+y0 = 0      # initial coordinates of robot
